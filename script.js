7a220ce348c9fba5fba664746062be963f8c65f9
unknown <Bryan@SJ-BHERRERA-XPS.netgear.com>
2021-01-03 16:57:54 -0800

    place apple and move snake dX

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let canvas = document.getElementById('gameCanvas');
 let canvasContext = canvas.getContext('2d');
+let canvasWidth = 400;
+let canvasHeight = 400;
+let framesPerSecond = 1000 / 10;
+let snakeXCoordinate = 50;
+let snakeYCoordinate = 10;
+let appleXCoordinate = Math.floor(Math.random() * 381) + 10;
+let appleYCoordinate = Math.floor(Math.random() * 381) + 10;
 
-//snake
-canvasContext.beginPath();
-canvasContext.rect(100, 100, 50, 10);
-canvasContext.fillStyle = 'red';
-canvasContext.fill();
-canvasContext.closePath();
+setInterval(function() {
+	moveSnake();
+	placeApple();
+}, framesPerSecond);
 
-//apple
-canvasContext.beginPath();
-canvasContext.arc(240, 160, 10, 0, Math.PI * 2, false);
-canvasContext.fillStyle = 'green';
-canvasContext.fill();
-canvasContext.closePath();
+function moveSnake() {
+	//snake
+	canvasContext.clearRect(0, 0, canvas.clientWidth, canvas.height);
+	canvasContext.beginPath();
+	canvasContext.fillStyle = 'red';
+	canvasContext.fillRect(snakeXCoordinate, 100, 50, 10);
+	canvasContext.fill();
+	canvasContext.closePath();
+	snakeXCoordinate += 10;
+}
+
+function placeApple() {
+	//apple
+
+	canvasContext.beginPath();
+	canvasContext.fillStyle = 'green';
+	canvasContext.arc(appleXCoordinate, appleYCoordinate, 10, 0, Math.PI * 2, false);
+	canvasContext.fill();
+	canvasContext.closePath();
+}
