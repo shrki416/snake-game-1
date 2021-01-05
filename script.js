94579dff7b7cd2c15260c95fc294cd8c4be9c194
unknown <Bryan@SJ-BHERRERA-XPS.netgear.com>
2021-01-05 07:19:39 -0800

    draw snake with variable body length

--- script.js
+++ script.js
@@ -3,33 +3,42 @@ let canvasContext = canvas.getContext('2d');
 let canvasWidth = 400;
 let canvasHeight = 400;
 let framesPerSecond = 1000 / 10;
-let snakeXCoordinate = 50;
-let snakeYCoordinate = 10;
+let snakeXOrigin = 50;
+let snakeYOrigin = 100;
 let appleXCoordinate = Math.floor(Math.random() * 381) + 10;
 let appleYCoordinate = Math.floor(Math.random() * 381) + 10;
+let snakeHeadOrigin = [ snakeXOrigin, snakeYOrigin ];
 
 setInterval(function() {
-	moveSnake();
+	canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+	drawSnakeChain(4);
+	// moveSnake();
 	placeApple();
 }, framesPerSecond);
 
-function moveSnake() {
-	//snake
-	canvasContext.clearRect(0, 0, canvas.clientWidth, canvas.height);
+function drawSnakeChain(snakeBodyLength) {
 	canvasContext.beginPath();
+	canvasContext.rect(snakeHeadOrigin[0], snakeHeadOrigin[1], 10, 10);
+	// for (let i = 1; i <= snakeBodyLength; i++) {
+	// 	canvasContext.rect(snakeHeadOrigin[0] - 11 * i, snakeHeadOrigin[1], 10, 10);
+	// }
 	canvasContext.fillStyle = 'red';
-	canvasContext.fillRect(snakeXCoordinate, 100, 50, 10);
 	canvasContext.fill();
 	canvasContext.closePath();
-	snakeXCoordinate += 10;
 }
+
+function moveSnake() {
+	snakeHeadOrigin[0] += 10;
+}
+
+function directSnakeMovement() {}
 
 function placeApple() {
 	//apple
-
 	canvasContext.beginPath();
 	canvasContext.fillStyle = 'green';
-	canvasContext.arc(appleXCoordinate, appleYCoordinate, 10, 0, Math.PI * 2, false);
+	canvasContext.arc(appleXCoordinate, appleYCoordinate, 7, 0, Math.PI * 2, false);
 	canvasContext.fill();
 	canvasContext.closePath();
 }
